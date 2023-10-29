79c3553c5a5c3ee4efe144a52cd23c3007fdfdc5
Kevin <kevhho@my.yorku.ca>
2023-10-29 17:05:39 -0400

    Task 1 and Task 2 implemented

--- app.py
+++ app.py
@@ -7,16 +7,23 @@ app.config.from_pyfile(  # flask's version of the servlet context param name+val
 app.config["SECRET_KEY"] = "test123"
 
 
-@app.route("/")
+@app.route("/", methods=["POST", "GET"])
 def index():
-    price = float(request.args.get("price", app.config["DEFAULT_PRICE"])) # price is either provided in param "&price=x" or uses our default in our "context" config.py
-    no_items = int(request.args.get("no_items", app.config["DEFAULT_NO_ITEMS"]))
-    if "tax" in request.args:  # we check if we see 'tax' in the param line
-        session["tax"] = float(request.args["tax"]) # if it is, we save that value to our "session", so testing curl commands, tax will stay at the provided param rate
-    tax = session.get("tax", app.config["DEFAULT_TAX"])
+    return render_template("index.html")
+
+
+@app.route("/shoppingcart", methods=["POST", "GET"])
+def shopping_cart():
+    price = float(
+        request.form.get(
+            "price", app.config["DEFAULT_PRICE"]
+        )  # price is either provided in param "&price=x" or uses our default in our "context" config.py
+    )
+    no_items = int(request.form.get("no_items", app.config["DEFAULT_NO_ITEMS"]))
+    tax = int(request.form.get("tax", app.config["DEFAULT_TAX"]))
+    # tax = session.get("tax", app.config["DEFAULT_TAX"])
 
     total = no_items * price * (1 + tax / 100)
-
     return render_template(
-        "index.html", price=price, tax=tax, no_items=no_items, total=total
+        "shoppingcart.html", price=price, tax=tax, no_items=no_items, total=total
     )
