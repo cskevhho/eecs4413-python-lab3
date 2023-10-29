868ac931114f0b55f5c623fdd4c8c9e5abb94824
Kevin <kevhho@my.yorku.ca>
2023-10-29 17:24:07 -0400

    Task 3 and Task 4 implemented

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, session
+from models.Cart import calculate_total
 
 app = Flask(__name__)
 app.config.from_pyfile(  # flask's version of the servlet context param name+value stuff
@@ -17,13 +18,15 @@ def shopping_cart():
     price = float(
         request.form.get(
             "price", app.config["DEFAULT_PRICE"]
-        )  # price is either provided in param "&price=x" or uses our default in our "context" config.py
+        )  # price is either provided in form or uses our default in our "context" config.py
     )
     no_items = int(request.form.get("no_items", app.config["DEFAULT_NO_ITEMS"]))
     tax = int(request.form.get("tax", app.config["DEFAULT_TAX"]))
-    # tax = session.get("tax", app.config["DEFAULT_TAX"])
 
-    total = no_items * price * (1 + tax / 100)
     return render_template(
-        "shoppingcart.html", price=price, tax=tax, no_items=no_items, total=total
+        "shoppingcart.html",
+        price=price,
+        tax=tax,
+        no_items=no_items,
+        total=calculate_total(no_items, price, tax),
     )
---
+++ models/Cart.py
@@ -0,0 +1,22 @@
+@staticmethod
+def validate_input(no_items: int, price: float, tax: float) -> str:
+    if no_items <= 0:
+        return "Number of items must be a positive integer."
+    if price <= 0:
+        return "Price must be a positive number."
+    if tax <= 0 or tax > 100:
+        return "Tax rate must be between 0 and 100"
+    return None  # null
+
+
+@staticmethod  # decorator indicates that this is a static method, don't need to call class.
+def calculate_total(
+    no_items: int, price: float, tax: float
+) -> float:  # type hinting, not forced, -> float means return float
+    validation_error = validate_input(no_items, price, tax)
+
+    if validation_error is not None:
+        raise ValueError(validation_error)
+
+    total = float(no_items * price * (1 + tax / 100))
+    return total
