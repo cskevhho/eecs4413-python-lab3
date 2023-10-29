bd6ee8dcd745fd004bd303f159be51eff4d8a265
Kevin <kevhho@my.yorku.ca>
2023-10-29 15:52:28 -0400

    Imported lab2 files

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
-app = Flask(  # So that this is run instead of any other library code we write on initialization.
-    __name__
+app = Flask(__name__)
+app.config.from_pyfile(  # flask's version of the servlet context param name+value stuff
+    "config.py"
 )
+app.config["SECRET_KEY"] = "test123"
 
 
 @app.route("/")
 def index():
-    return render_template(  # Flask will use render_template to find the passed in html template, dynamic variable assignment in the brackets.
-        "index.html"
+    price = float(request.args.get("price", app.config["DEFAULT_PRICE"])) # price is either provided in param "&price=x" or uses our default in our "context" config.py
+    no_items = int(request.args.get("no_items", app.config["DEFAULT_NO_ITEMS"]))
+    if "tax" in request.args:  # we check if we see 'tax' in the param line
+        session["tax"] = float(request.args["tax"]) # if it is, we save that value to our "session", so testing curl commands, tax will stay at the provided param rate
+    tax = session.get("tax", app.config["DEFAULT_TAX"])
+
+    total = no_items * price * (1 + tax / 100)
+
+    return render_template(
+        "index.html", price=price, tax=tax, no_items=no_items, total=total
     )
-
-
-@app.route("/result")
-def result():
-    # name = request.args.get("name", "world")
-    # return render_template("result.html")           # These two lines are the same thing as the single line return below.
-    return render_template("result.html", name=request.args.get("name", "world"))
